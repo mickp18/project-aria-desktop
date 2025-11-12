bb6c4cad28a8e802216febfc39da5d60b18199dc
mickp18 <miperseo@gmail.com>
2025-11-12 16:42:15 +0100

    move start application logic in server

--- aria_desktop/__main__.py
+++ aria_desktop/__main__.py
@@ -10,67 +10,18 @@ from .utils.logger import logger
 from .utils.config import config
 from .bus import AsyncEventBus
 from .workers.local_yolo import yolo_worker
+from .server.server import WebSocketServer
 
 async def main():
     """Main function to run the desktop app."""
-    logger.info("Starting Project Aria Desktop App")
 
-    streaming_handler = None
+    logger.info("Starting Project Aria Desktop App")
     evnet_bus = AsyncEventBus()
 
-    http_client = httpx.AsyncClient(timeout=5.0) # 5 sec timeout
+    server = WebSocketServer(evnet_bus)
+    await server.start()
+
     
-    loop = asyncio.get_running_loop()
-
-    # Start the worker task
-    worker_task = asyncio.create_task(yolo_worker(evnet_bus, http_client))
-
-    try:
-        # Initialize the client (it will load settings from config.ini)
-        client = AriaClient()
-        
-        # Start the pairing process
-        # await client.pair()
-        
-        # Connect to the device
-        device = await client.connect()
-        
-        if device:
-            logger.info("Successfully connected to the Aria device.")
-          
-            battery_level = client.get_battery_level(device)
-            logger.info(f"Battery level : {battery_level}%")
-
-            if battery_level < config.getint('streaming', 'min_battery_level', fallback=20):
-                logger.warning("Battery level is below 20%. Please charge the device soon.")
-            else :
-                logger.info("Battery level is sufficient, ready to go.")
-
-                # start streaming
-                streaming_handler = StreamingHandler(device, evnet_bus, loop)
-                await streaming_handler.start_streaming()
-
-        else:
-            logger.error("Could not connect to device. Exiting.")
-
-        
-    except (asyncio.CancelledError, KeyboardInterrupt):
-        logger.info("Application interrupted. Shutting down...")
-    except Exception as e:
-        logger.critical(f"An error occurred during application startup: {e}")
-    finally:
-        # --- CLEAN UP WORKER AND HTTP CLIENT ---
-        logger.info("Cleaning up tasks and connections...")
-        if worker_task:
-            worker_task.cancel()
-        if http_client:
-            await http_client.aclose()
-
-        if streaming_handler:
-            logger.info("Main ensuring stream is stopped.")
-            # This call is synchronous and cleans up
-            streaming_handler.stop_streaming() 
-        logger.info("Application has shut down.")
 
 if __name__ == "__main__":
     try:
--- aria_desktop/server/server.py
+++ aria_desktop/server/server.py
@@ -6,24 +6,87 @@ from typing import Set
 from ..utils.logger import logger
 from ..bus import AsyncEventBus, Event
 from ..utils.config import config
+from ..core.client import AriaClient
+from ..core.streaming_handler import StreamingHandler
+from ..workers.websocket_worker import websocket_worker as ws_worker
 
 
 class WebSocketServer:
     def __init__(self, bus: AsyncEventBus):
         self.bus = bus
         self.port = config.getint('websocket', 'port', fallback=8088)
-
+        self.task : asyncio.Task | None = None
+        self.stop = asyncio.Event() 
         # Allow possible multiple clients connections 
         # self.connected_clients = set()
         self.connected_client = None
+    
+    async def _run_app(self):
+        """Main application logic to run the WebSocket server and handle connections."""
+        _ws_worker = ws_worker(self.bus, self)
+        streaming_handler = None
         
+        loop = asyncio.get_running_loop()
+
+        # Start the worker task
+        worker_task = asyncio.create_task(_ws_worker.forward_rgb())
+
+        try:
+            # Initialize the client (it will load settings from config.ini)
+            client = AriaClient()
+            
+            # Start the pairing process
+            # await client.pair()
+            
+            # Connect to the device
+            device = await client.connect()
+            
+            if device:
+                logger.info("Successfully connected to the Aria device.")
+            
+                battery_level = client.get_battery_level(device)
+                logger.info(f"Battery level : {battery_level}%")
+
+                if battery_level < config.getint('streaming', 'min_battery_level', fallback=20):
+                    logger.warning("Battery level is below 20%. Please charge the device soon.")
+                else :
+                    logger.info("Battery level is sufficient, ready to go.")
+
+                    # start streaming
+                    streaming_handler = StreamingHandler(device, self.bus, loop)
+                    await streaming_handler.start_streaming()
+
+            else:
+                logger.error("Could not connect to device. Exiting.")
+
+            
+        except (asyncio.CancelledError, KeyboardInterrupt):
+            logger.info("Application interrupted. Shutting down...")
+        except Exception as e:
+            logger.critical(f"An error occurred during application startup: {e}")
+        finally:
+            # --- CLEAN UP WORKER AND HTTP CLIENT ---
+            logger.info("Cleaning up tasks and connections...")
+            if worker_task:
+                worker_task.cancel()
+    
+            if streaming_handler:
+                logger.info("Main ensuring stream is stopped.")
+                # This call is synchronous and cleans up
+                streaming_handler.stop_streaming() 
+            logger.info("Application has shut down.")
+
     async def handle_message(self, message: str):
         """Process incoming messages from clients."""
         logger.debug(f"Handling message: {message}")
 
         if message.lower() == "start":
-            event = Event(event_type="start_command")
-            await self.bus.publish(event)
+            logger.debug("Received start command from client")
+            # event = Event(event_type="start_command")
+            # await self.bus.publish(event)
+            self.stop = False
+            self.task = asyncio.create_task(self._run_app())
+            
 
         elif message.lower() == "stop":
             event = Event(event_type="stop_command")
